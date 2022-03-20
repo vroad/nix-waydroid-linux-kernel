7855452e7fb9696bfe8ba8c7357a58ef9823b345
vroad <396351+vroad@users.noreply.github.com>
2022-03-20 10:43:11 +0000

    Add linux_5_10

--- default.nix
+++ default.nix
@@ -1,1 +1,7 @@
-{ }
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+in
+{
+  linux_5_10 = pkgs.callPackage ./pkgs/os-specific/linux/kernel/linux-5.10.nix { };
+}
---
+++ pkgs/os-specific/linux/kernel/linux-5.10.nix
@@ -0,0 +1,11 @@
+{ linux_5_10 }:
+
+linux_5_10.override (originalArgs: {
+  kernelPatches = originalArgs.kernelPatches ++ [{
+    name = "waydroid-config";
+    patch = null;
+    extraConfig = ''
+      CGROUP_CPUACCT y
+    '';
+  }];
+})
